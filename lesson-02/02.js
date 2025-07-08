d47a2df3801a737be2c637c1b05e82428093b695
vladKrival <vlad.krival.8@gmail.com>
2025-07-08 23:16:37 +0300

    complete-4

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,9 +7,22 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1 = Math.floor(Math.random() * 6) + 1; // your code
+console.log(dice1)
+let dice2 = Math.floor(Math.random() * 6) + 1; // your code
+console.log(dice2)
+
+let isWinningDouble = false // your code
+
+
+// while(!isWinningDouble) {
+  if (dice1 === dice2 && dice1 > 3)  {
+    // isWinningDouble = true
+  console.log('Выигрышный дубль!')
+} else {
+  console.log('Не выигрышный дубль.')
+}
+// }
 
 /*
 console.log('Первый бросок: ' + dice1)
