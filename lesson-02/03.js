65aae875509b172559b78ae92134ef89bf73e1ee
vladKrival <vlad.krival.8@gmail.com>
2025-07-11 13:54:18 +0300

    complete-1,2,3

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,8 +4,22 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
+const score = 50 // тестовое значение, можно изменять
 let grade
 
 
+if(score >= 0 && score <= 49) {
+    grade = 'F'
+  } else if(score >= 50 && score <= 69) {
+    grade = 'D'
+  } else if(score >= 70 && score <= 79) {
+    grade = 'C'
+  } else if(score >= 80 && score <= 89) {
+    grade = 'B'
+  } else if(score >= 90 && score <= 100) {
+    grade = 'A'
+  } else {
+    console.log('Введите балл от 0-100')
+  }
 // your code
+console.log( 'Твоя оценка ' + grade);
