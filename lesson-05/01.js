6b8449303a6d7f43c8e43d18c3d3d599b9af22bc
vladKrival <vlad.krival.8@gmail.com>
2025-07-28 15:21:33 +0300

    complete 5.1

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -15,5 +15,15 @@ const game = {
     gold: 250,
     lumber: 100,
   },
-  addResource() {}
-}
+  addResource: function(resource, amount) {
+    if(!(resource in this.resources)){
+      console.log('invalid resource');
+      return;
+    }
+    this.resources[resource] += amount;
+  }
+};
+
+game.addResource('gold', 50)
+game.addResource('lumber', 100)
+console.log(game)
