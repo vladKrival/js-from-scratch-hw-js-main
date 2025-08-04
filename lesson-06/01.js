1852a4d894d7453c1634deac7bbe85b9279b4291
vladKrival <vlad.krival.8@gmail.com>
2025-08-04 23:34:31 +0300

    complete task-6.1

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -23,4 +23,22 @@ const oddNumbers = filter(numbers, (element, index) => {
 console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 */
 
-const filter = () => {}
+const numbers = [1, 2, 3, 4, 5]
+
+
+const filter = (array, callback) => {
+  const result = [];
+  for (let i = 0; i < array.length; i++){
+    if (callback(array[i], i)){
+      result.push(array[i])
+    }
+  }
+  return result
+}
+
+
+const oddNumbers = filter(numbers, (element, index) => {
+  return element % 2 === 0;
+});
+
+console.log(oddNumbers);
--- lesson-06/02.js
+++ lesson-06/02.js
@@ -24,4 +24,19 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+const numbers = [1, 2, 3, 4, 5]
+
+const map = (array, fun ) => {}
+
+
+const doubleNumbers = (array, i) => {
+for (let i = 0; i < array.length; i++){
+  return i * 2
+}
+}
+
+
+
+const doubledNumbers = map(numbers, (element, index) => {
+  return element * 2
+})
