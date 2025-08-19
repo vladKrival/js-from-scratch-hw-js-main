4848797df3427cc5c2a4f61205baa94ef3355eb4
vladKrival <vlad.krival.8@gmail.com>
2025-08-19 21:52:46 +0300

    complete8.1

--- lesson-08/01.js
+++ lesson-08/01.js
@@ -6,4 +6,26 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+const text = 'hello world from javascript'
+
+function capitalizeWords(text) {
+  let result = '';
+  let makeUpper = true; // нужно ли сделать след букву заглавной
+
+  for (let i = 0; i < text.length; i++){
+    let sentence = text[i]; // берем текущий символ строки
+    if(makeUpper){
+      result += sentence.toUpperCase(); //делаем заглавной буквой
+      makeUpper = false;
+      } else {
+        result += sentence;
+      }
+      if (sentence === ' '){
+        makeUpper = true;
+      }
+  }
+  return result;
+}
+
+const capitalizedText = capitalizeWords(text)
+console.log(capitalizedText)
