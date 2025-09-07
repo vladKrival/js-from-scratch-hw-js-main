e9cb9d8ac7d8e04c29373a069875da72cc59993b
vladKrival <vlad.krival.8@gmail.com>
2025-09-07 19:17:25 +0300

    complete-8.2

--- lesson-08/02.js
+++ lesson-08/02.js
@@ -4,11 +4,12 @@
  */
 
 function isNumeric(str) {
-  // your code
+  return str.trim() !== " " && !isNaN(str);
 }
 
-// console.log(isNumeric("123")) // Ожидаемый результат: true
-// console.log(isNumeric("12.3")) // Ожидаемый результат: true
-// console.log(isNumeric("123abc")) // Ожидаемый результат: false
+
+console.log(isNumeric("123")) // Ожидаемый результат: true
+console.log(isNumeric("12.3")) // Ожидаемый результат: true
+console.log(isNumeric("123abc")) // Ожидаемый результат: false
 // console.log(isNumeric("abc")) // Ожидаемый результат: false
 // console.log(isNumeric(" ")) // Ожидаемый результат: false
